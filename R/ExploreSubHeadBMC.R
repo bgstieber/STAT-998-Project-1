6b0ccb3c9b23aea693f7baa367194fc67f46c944
bgstieber <bgstieber@gmail.com>
2016-10-02 12:43:10 -0500

    added plots comments code

--- R/ExploreSubHeadBMC.R
+++ R/ExploreSubHeadBMC.R
@@ -72,6 +72,21 @@ ggplot(gym, aes(x = Chronologic.Age.at.Menarche, y = Sub.head.BMC))+
                   colour = ChronAgeAtMenarche_Group),
               method = 'lm', se = F, size = 2)
 
+ggplot(gym, aes(x = Chronologic.Age.at.Menarche, y = Sub.head.BMC))+
+  geom_jitter(aes(colour = ChronAgeAtMenarche_Group), pch = 1)+
+  geom_smooth(aes(group = ChronAgeAtMenarche_Group,
+                  colour = ChronAgeAtMenarche_Group),
+              method = 'lm', se = F, size = 2)+
+  xlab('Chronologic Age at Menarche')+
+  ylab('Sub head BMC')+
+  scale_colour_brewer(palette = 'Dark2',
+                      name = 'Age at Menarche Group')+
+  ggtitle('Age at Menarche by Sub head BMC')+
+  theme(legend.position = 'top',
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 14),
+        legend.margin = unit(0, 'cm'))
+
 ggplot(gym, aes(x = Group_Label, y = Chronologic.Age.at.Menarche))+
   geom_violin(draw_quantiles = c(.25, .5, .75))+
   stat_summary(fun.y = 'mean', size = 2, col = 'blue', geom = 'point')
@@ -108,7 +123,7 @@ ggplot(gym, aes(x = YearsAfterGymQuit, y = Sub.head.BMC))+
   geom_jitter()
 
 gym$Group_Label2 <- ifelse(is.na(gym$YearsAfterGymQuit), 'Never',
-                           ifelse(gym$YearsAfterGymQuit >= (45 / 365), 'Quit',
+                           ifelse(gym$YearsAfterGymQuit >= (90 / 365), 'Quit',
                                   'In Gymnastics'))
 
 #enter and interact with Age?
@@ -209,6 +224,7 @@ ggplot(gym_sub_nogroup, aes(x = Standing.Height, y = value))+
 
 ggplot(gym_sub_nogroup, aes(x = value))+
   stat_function(fun = 'dnorm', colour = 'red', size = 2)+
+  ylab('Standardized Values (zero meaun, unit variance)')+
   geom_density(aes(group = variable), alpha = .2)+
   ggtitle('Centered and Scaled Variable Density Plots',
           subtitle = 'Red Curve is the Standard Normal Density')
@@ -222,7 +238,22 @@ ggplot(gym_sub_base, aes(x = value))+
         strip.text = element_text(face = 'bold'),
         strip.background = element_rect(fill = 'white'))
 
-#random slope for standing height?
+#plot individual trajectories for each response
+
+
+ggplot(gym_sub_nogroup, aes(x = Sub.Head.LM, y = value))+
+  geom_line(aes(group = ID), alpha = .3)+
+  facet_wrap(~variable)+
+  xlab('Sub Head LM')+
+  ylab('Standardized Value (zero mean, unit variance)')+
+  ggtitle('Individual Trajectories for Each Response Variable')
+
+ggplot(gym_sub_nogroup, aes(x = Menarcheal.Age.at.DXA, y = value))+
+  geom_line(aes(group = ID), alpha = .3)+
+  facet_wrap(~variable)+
+  xlab('Menarcheal Age at DXA')+
+  ylab('Standardized Value (zero mean, unit variance)')+
+  ggtitle('Individual Trajectories for Each Response Variable')
 
 ggplot(gym_sub_nogroup, aes(x = Standing.Height, y = value))+
   geom_line(aes(group = ID), alpha = .3)+
@@ -230,6 +261,8 @@ ggplot(gym_sub_nogroup, aes(x = Standing.Height, y = value))+
   xlab('Standing Height')+
   ylab('Standardized Value (zero mean, unit variance)')+
   ggtitle('Individual Trajectories for Each Response Variable')
+
+
 
 # #create a new age group variable, just three levels
 # gym$ChronAgeAtMenarche_Group2 <- cut(gym$Chronologic.Age.at.Menarche,
@@ -253,3 +286,87 @@ summary(fit3.gs <- gamm4(Sub.head.BMC ~ Sub.Head.LM + Standing.Height + Group_La
                          random = ~(1 + Standing.Height | ID)))
 #fit a cubic to it
 fit3.cubicage <- update(fit3, .~. + I(Menarcheal.Age.at.DXA ^ 2) + I(Menarcheal.Age.at.DXA^3))
+
+do.call('data.frame', 
+    aggregate(value ~ Group_Label + variable, function(x)
+      paste0(round(mean(x, na.rm = T), 2), 
+             " (", 
+             round(sd(x, na.rm = T), 2), ")"), 
+      data = gym_sub_base)) -> df1_base
+
+reshape(df1, idvar = 'variable', timevar = 'Group_Label', direction = 'wide') -> df2
+
+##presentation plots
+df1_cs <- structure(list(variable = structure(c(1L, 2L, 3L, 4L, 5L, 6L, 
+                                                7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 1L, 2L, 3L, 4L, 5L, 6L, 
+                                                7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 1L, 2L, 3L, 4L, 5L, 6L, 
+                                                7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L), .Label = c("Sub.head.BMC", 
+                                                                                                 "Distal.Radius.Third.Area", "Distal.bone.mineral.content.third", 
+                                                                                                 "Distal.Modulus.Third.Radius", "UD.Area.Radius", "UD.BMC", "UD.IBS.Radius", 
+                                                                                                 "Femoral.Neck.BMC.Hip", "NN.Section.Modulus.Hip", "NN.BR.Hip", 
+                                                                                                 "NN.width.Hip", "NN.ED.Hip", "NN.ACT.Hip", "PA.L3.BMC.Spine"), class = "factor"), 
+                         Group_Label = structure(c(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+                                                   1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 
+                                                   2L, 2L, 2L, 2L, 2L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 
+                                                   3L, 3L, 3L, 3L), .Label = c("NON", "PERI", "POST"), class = "factor"), 
+                         value = c(-0.061472977396549, -0.432321421767705, -0.384652606357142, 
+                                   -0.433231766287853, -0.356155532928317, -0.430769287128757, 
+                                   -0.374326524362265, -0.13616416536045, -0.0647393890113162, 
+                                   0.309450539340368, 0.203860003705822, 0.27996831602529, -0.261525656205867, 
+                                   -0.0614109381209648, -0.16096273244324, 0.29747685374581, 
+                                   0.123460668416626, 0.203153823446502, 0.587537528475897, 
+                                   0.318209902034928, 0.0615222913491079, -0.151322191741991, 
+                                   -0.0817516550842374, -0.120499870897454, -0.189210976836825, 
+                                   -0.152502978970673, -0.0557624217988491, -0.175098754834998, 
+                                   0.224473739108582, 0.760485004519492, 0.748652058521092, 
+                                   0.810510576909655, 0.451844034956042, 0.746636145803863, 
+                                   0.758452597816335, 0.389141836727661, 0.189469273699493, 
+                                   -0.639792791435959, -0.364372500457071, -0.556226528312661, 
+                                   0.622659698922718, 0.231682400811208)), .Names = c("variable", 
+                                                                                      "Group_Label", "value"), row.names = c(NA, -42L), class = "data.frame")
+
+
+merge(df1_base, df1_cs, by = c('variable','Group_Label')) -> df1_merge
+
+factor_levels <- c("Sub.head.BMC", "Distal.Radius.Third.Area", "Distal.bone.mineral.content.third", 
+                   "Distal.Modulus.Third.Radius", "UD.Area.Radius", "UD.BMC", "UD.IBS.Radius",
+                   "PA.L3.BMC.Spine","NN.ACT.Hip", "Femoral.Neck.BMC.Hip", "NN.Section.Modulus.Hip", "NN.BR.Hip", 
+                   "NN.width.Hip", "NN.ED.Hip")
+
+df1_merge$variable_f <- factor(df1_merge$variable, levels = rev(factor_levels))
+
+ggplot(df1_merge, aes(x = Group_Label, y = variable_f))+
+  geom_tile(aes(fill = value.y), colour = 'black')+
+  geom_text(aes(label = value.x), size = 6)+
+  scale_fill_gradient(low = 'white', high = 'grey60')+
+  theme(legend.position = 'none',
+        axis.title = element_blank(),
+        panel.grid = element_blank(),
+        axis.text = element_text(size = 14))
+
+
+ggplot(gym_sub_nogroup, aes(x = value))+
+  stat_function(fun = 'dnorm', colour = 'red', size = 2)+
+  ylab('Standardized Values (zero meaun, unit variance)')+
+  geom_density(aes(group = variable), alpha = .2)+
+  ggtitle('Centered and Scaled Variable Density Plots',
+          subtitle = 'Red Curve is the Standard Normal Density')
+
+ggplot(gym, aes(x = Chronologic.Age.at.Menarche, y = Sub.head.BMC))+
+  geom_jitter(aes(colour = ChronAgeAtMenarche_Group), pch = 1)+
+  geom_smooth(aes(group = ChronAgeAtMenarche_Group,
+                  colour = ChronAgeAtMenarche_Group),
+              method = 'lm', se = F, size = 2)+
+  xlab('Chronologic Age at Menarche')+
+  ylab('Sub head BMC')+
+  scale_colour_brewer(palette = 'Dark2',
+                      name = 'Age at Menarche Group')+
+  ggtitle('Age at Menarche by Sub head BMC')+
+  theme(legend.position = 'top',
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 14),
+        legend.margin = unit(0, 'cm'),
+        plot.title = element_text(size = 18),
+        legend.title = element_text(size = 16)) 
+
+
